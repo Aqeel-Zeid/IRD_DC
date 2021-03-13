69feeba0fa4331f638551d94cf66aa47eb2b2143
Mohamed Rifan <mohamedrifan@live.com>
2021-03-13 18:29:21 +0530

    Created Questionaire List Display

--- ird-front/src/components/Pages/QExplorer.jsx
+++ ird-front/src/components/Pages/QExplorer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NewQuestionaireButton from '../NewQuestionaireButton'
+import QCard from '../QCard'
+import QCardArray from '../QCardArray'
 
 export default function QExplorer() {
     return (
         <div>
             <NewQuestionaireButton/>
+            <h1>Drafts</h1>
+            <QCardArray/>
         </div>
     )
 }
