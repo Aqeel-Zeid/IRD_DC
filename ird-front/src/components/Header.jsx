030bfd626b89c3da374cabda3a0c09ddcca692c4
Mohamed Rifan <mohamedrifan@live.com>
2021-03-15 13:05:55 +0530

    Fixed Header Styles margin and Text Input

--- ird-front/src/components/Header.jsx
+++ ird-front/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header({Title,Description}) {
     return (
-        <div style = {{marginTop:"16px"}}>
+        <div>
             <h1>{Title}</h1>
             <label className="TitleDescription">{Description}</label>
         </div>
--- ird-front/src/components/TextInput.jsx
+++ ird-front/src/components/TextInput.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
-export default function TextInput({ ClassName, Label, InputType, PlaceHolder }) {
-    return (
-        <div>
+export default function TextInput({
+  Label,
+  InputType,
+  PlaceHolder,
+  OnChangeMethod,
+  FieldValue,
+}) {
+  //
 
-            <label>{Label}</label>
-            <InputType className={ClassName} placeholder={PlaceHolder}/>
-        </div>
-    )
+  return (
+    <div>
+      <label>{Label}</label>
+      {InputType === "textArea" ? (
+        <textarea 
+            placeholder={PlaceHolder} 
+            className="TextArea" 
+            onChange = { (e) => OnChangeMethod(e) }
+            value = {FieldValue}
+            />
+      ) : (
+        <input 
+            className="TextInput" 
+            placeholder={PlaceHolder} 
+            onChange = { (e) => OnChangeMethod(e) }
+            value = {FieldValue}
+            />
+      )}
+    </div>
+  );
 }
