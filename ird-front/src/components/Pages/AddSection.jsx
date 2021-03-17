892f7d325b0c00ed9e72e7229c3b7a471c0f0335
Mohamed Rifan <mohamedrifan@live.com>
2021-03-17 09:36:05 +0530

    Normal Section UI done

---
+++ ird-front/src/components/Pages/AddSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import QuestionDivider from "../QuestionDivider";
+import BigIconButton from "../BigIconButton";
+import addFile from "./Assets/add_file.svg";
+import libraryIcon from "./Assets/library.svg";
+import { Link } from "react-router-dom";
+
+export default function AddSection() {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <BigIconButton
+        Icon={libraryIcon}
+        IconDescription="Add To Library Icon"
+        Label="Select From Library"
+        Description="Select from an already existing questions library"
+      />
+      <QuestionDivider
+        ClassName="VerticalDivider"
+        ClassNameDividerLabel="VerticalDividerLabel"
+      />
+      <BigIconButton
+        Icon={addFile}
+        IconDescription="Create New Question Icon"
+        Label="Create new question"
+        Description="Create new question"
+        LinkTo="/CQ/AQ/QuestionFormat"
+      />
+    </div>
+  );
+}
